c9f8f1f2da8c550ed3c293b5f2325c5b65d22fe4
Rosie Hood <rosie.hood@hotmail.co.uk>
2019-12-02 21:49:21 +0000

    day2

--- day02.py
+++ day02.py
@@ -4,32 +4,34 @@ def read_data(filename='data/day02.txt'):
     return program
 
 
-def intcode(ints):
+def intcode(program):
+    ints = program.copy()
     idx = 0
-    while idx < len(ints):
-        if ints[idx] == 1:
-            ints[ints[idx + 3]] = ints[ints[idx + 1]] + ints[ints[idx + 2]]
-        elif ints[idx] == 2:
-            ints[ints[idx + 3]] = ints[ints[idx + 1]] * ints[ints[idx + 2]]
+    while ints[idx] != 99:
+        opcode, param1, param2, output = ints[idx:idx + 4]
+        if opcode == 1:
+            ints[output] = ints[param1] + ints[param2]
+        elif opcode == 2:
+            ints[output] = ints[param1] * ints[param2]
         idx += 4
     return ints[0]
 
 
-def restore_gravity(ints, noun=12, verb=2):
-    ints[1] = noun
-    ints[2] = verb
-    return ints
+def restore_gravity(program, noun=12, verb=2):
+    program[1] = noun
+    program[2] = verb
+    return program
 
 
-def find_noun_verb(ints):
+def find_noun_verb(program):
     for noun in range(100):
         for verb in range(100):
-            if intcode(restore_gravity(ints[:], noun, verb)) == 19690720:
+            if intcode(restore_gravity(program, noun, verb)) == 19690720:
                 return 100 * noun + verb
 
 
 if __name__ == '__main__':
 
     program = read_data()
-    print(intcode(restore_gravity(program[:])))
+    print(intcode(restore_gravity(program)))
     print(find_noun_verb(program))
